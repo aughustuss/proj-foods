6558384c0db39457448c84de40c227b99081f2c1
augusto <augusdpmenezes@gmail.com>
2023-06-16 19:42:52 -0300

    responsividade da navbar

--- projeto-foods/src/app/pages/login/login.component.ts
+++ projeto-foods/src/app/pages/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { FormGroup, FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { NgToastService } from 'ng-angular-popup'
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { StoreService } from 'src/app/services/store/store.service';
 import ValidateForm from 'src/app/helpers/validateform';
@@ -10,28 +9,33 @@ import {MatButtonModule} from '@angular/material/button'
 import { CommonModule } from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSnackBarModule, MatSnackBar, MatSnackBarRef} from '@angular/material/snack-bar'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
-  imports: [MatButtonModule, RouterModule, CommonModule, FormsModule, ReactiveFormsModule, MatInputModule, MatFormFieldModule]
+  imports: [MatButtonModule, RouterModule, CommonModule, FormsModule, ReactiveFormsModule, MatInputModule, MatFormFieldModule, MatSnackBarModule]
 })
+
 export class LoginComponent implements OnInit {
   public loginForm!: FormGroup;
   type: string = 'password';
   isText: boolean = false;
   eyeIcon: string = 'fa-eye-slash';
   isOpen: boolean = false;
+  loginErrorMessage: string = '';
+  snackDuration: number = 5;
   public resetPasswordEmail: string = '';
   public isValidEmail: boolean = false;
+
   constructor(
     private fb: FormBuilder,
     private auth: AuthService,
     private route: Router,
-    private toast: NgToastService,
     private userStore: StoreService,
-    private resetPassword: ResetpasswordService
+    private resetPassword: ResetpasswordService,
+    private snackBar: MatSnackBar,
   ) { }
 
   ngOnInit(): void {
@@ -54,18 +58,19 @@ export class LoginComponent implements OnInit {
     if (this.loginForm.valid) {
       this.auth.logIn(this.loginForm.value).subscribe({
         next: (res) => {
-          console.log(res);
           this.auth.storeToken(res.accessToken);
           this.loginForm.reset();
           this.auth.storeRefreshToken(res.refreshToken);
           const userPayload = this.auth.decodifyToken();
           this.userStore.setFullNameForStore(userPayload.unique_name);
           this.userStore.setRoleForStore(userPayload.role);
-          this.toast.success({ detail: "SUCCESS.", summary: res.message, duration: 5000 });
+          this.snackBar.open("Login realizado com sucesso.", "Ok", {
+            duration: this.snackDuration * 1000,
+          })
           this.route.navigate(['']);
         },
         error: (err) => {
-          this.toast.error({ detail: "ERROR.", summary: err.message, duration: 5000 });
+          this.loginErrorMessage = 'Email ou senha inválidos.';
           console.log(err);
         }
       });
@@ -86,7 +91,9 @@ export class LoginComponent implements OnInit {
         next: (res) => {
           this.resetPasswordEmail = '';
           this.isOpen = !this.isOpen;
-          alert("Email enviado com sucesso.");
+          this.snackBar.open("Email enviado com sucesso.", "Ok", {
+            duration: this.snackDuration * 1000,
+          });
         },
         error: (e) => {
           console.log("Erro:", e);
@@ -95,4 +102,3 @@ export class LoginComponent implements OnInit {
     }
   }
 }
-
